091364209bb00917d35c7c96938ff4da08a13ca8
KittyC513 <shuichunchenkitty@gmail.com>
2022-11-02 01:22:39 -0400

    Added Spawn Function

--- Assets/MonsterSpawn.cs
+++ Assets/MonsterSpawn.cs
@@ -5,51 +5,77 @@ using UnityEngine;
 
 public class MonsterSpawn : MonoBehaviour
 {
-    public GameObject monsterPrefab;
+    public GameObject[] monsterPrefab;
     public GameObject player;
-    public GameObject spawnPoint;
 
+    public float radius;
 
-    bool isSpawned = false;
+    public bool isSpawned;
 
     public float spawnTime;
     public float spawnDelay;
+
+    public int startWait;
+    public float spawnWait;
+
+    public float minTime;
+    public float maxTime;
+
+    Vector3 randomPos;
+    int randMonster;
+
+    public int spawnCount;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        StartCoroutine(waitSpawner());
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(43, -48), 2, Random.Range(48, -49));
-            Instantiate(monsterPrefab, randomSpawnPosition, Quaternion.identity);
-            Debug.Log(monsterPrefab.transform.position);
-        
-        } */
 
-        SpawnObject();
+        spawnWait = Random.Range(minTime, maxTime);
+
     }
 
 
-    private void SpawnObject()
+    private void SpawnFunction()
     {
-        //spawn a monster in a random position
-        if (!isSpawned)
+
+        randomPos = Random.insideUnitSphere * radius;
+        randomPos += player.transform.position;
+        randomPos.y = 0.3f;
+
+        Vector3 direction = randomPos - player.transform.position;
+        direction.Normalize();
+
+        float dotProduct = Vector3.Dot(player.transform.forward, direction);
+        float dotProductAngle = Mathf.Acos(dotProduct / player.transform.forward.magnitude * direction.magnitude);
+
+        randomPos.x = Mathf.Cos(dotProductAngle) * radius + player.transform.position.x;
+        randomPos.z = Mathf.Sin(dotProductAngle * (Random.value > 0.5 ? 1f : -1f)) * radius + player.transform.position.z;
+
+    }
+
+
+
+    IEnumerator waitSpawner()
+    {
+
+        yield return new WaitForSeconds(startWait);
+        SpawnFunction();
+       
+        for (int count = spawnCount; count > 0; count--)
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(43, -48), 2, Random.Range(48, -49));
-            Instantiate(monsterPrefab, randomSpawnPosition, Quaternion.identity);
-            isSpawned = true;
-        }
-        else
-        {
-            CancelInvoke("SpawnObject");
+            GameObject monster = Instantiate(monsterPrefab[randMonster], randomPos, Quaternion.identity);
+            monster.transform.position = randomPos;
+            yield return new WaitForSeconds(spawnWait);
         }
 
     }
 }
+    
+
